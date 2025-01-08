4a5ddb86e497eb475b145bb17a71671f2ec88730
palkamj <131778496+palkamj@users.noreply.github.com>
2025-01-08 13:19:54 +0200

    Create graph_functions.py and add streamlit to main

--- personnel-app/main.py
+++ personnel-app/main.py
@@ -1,7 +1,10 @@
 # main.py
 import os
+import streamlit as st
+import pandas as pd
 from get_data import get_dataframe
 from dotenv import load_dotenv
+from graph_functions import makeTreemap, makeSunkey, makeIcicle, makeSunburst
 
 load_dotenv()
 
@@ -23,7 +26,46 @@ def main():
     )
     
     # Now you can work with df here
-    print(df.head())
+    
+    df["EMPLOYEE_ID"] = df["EMPLOYEE_ID"].astype(str)
+
+    # Add full name
+    df["FULL_NAME"] = df["FIRST_NAME"].astype(str) + " " + df["LAST_NAME"].astype(str)
+
+    # Create a mapping from EMPLOYEE_ID to FULL_NAME
+    emp_id_to_name = df.set_index("EMPLOYEE_ID")["FULL_NAME"].to_dict()
+
+    # Map MANAGER_ID to manager name, handling missing or invalid manager IDs
+    df["MANAGER_ID"] = df["MANAGER_ID"].astype(str)
+    df["MANAGER_NAME"] = df["MANAGER_ID"].map(emp_id_to_name)
+
+    #print(df.head())
+
+
+    st.title("Hierarchical Data Viewer")
+
+    labels = df[df.columns[11]]
+    parents = df[df.columns[12]]
+
+    fig = makeTreemap(labels, parents)
+    st.plotly_chart(fig, use_container_width=True)
+
+    edges = ""
+    for _, row in df.iterrows():
+        if not pd.isna(row.iloc[12]):
+            edges += f'\t"{row.iloc[11]}" -> "{row.iloc[12]}";\n'
+
+
+
+    d = f'''
+    digraph {{
+        rankdir=LR;
+        {edges}
+    }}
+    '''
+    
+    st.graphviz_chart(d)
+
 
 if __name__ == "__main__":
     main()
